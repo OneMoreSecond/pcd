49bc3750c8aee81572b7dd68031443c0d2f4d8eb
Xianghao Tang <xianta@microsoft.com>
2019-12-26 16:54:18 +0800

    Improve the function example

--- function_example.py
+++ function_example.py
@@ -1,5 +1,7 @@
-def raw(*args):
+import os
+
+def raw(args):
     # receive string arguments from command line
-    # return a string to change to target directory
-    assert len(args) == 1, 'pcd function "raw" expects exact one argument'
-    return args[0]
+    # return a string of target path
+    assert len(args) >= 1, 'pcd function "raw" expects at least one argument'
+    return os.path.join(*args)
